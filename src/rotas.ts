460ff649c4b0f0adf241abbb3d35866b1f662879
Jamile Moreira <jamilemoreira2310@gmail.com>
2024-01-26 12:18:09 -0300

    confg base

---
+++ src/rotas.ts
@@ -0,0 +1,12 @@
+import { Router } from 'express'
+
+const rotas = Router()
+
+//listagem de carros
+//detalhes carro
+//cadastro carro
+//atualizacao carro
+//exclusao carro
+rotas.get('/')
+
+export default rotas
